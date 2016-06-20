7ddf5abc06966d532bb514e8a35b85657882bade
John Swanson <john@red.yellow.blue>
2016-06-20 16:39:48 -0400

    sequencer
    design update

--- script.js
+++ script.js
@@ -50,23 +50,30 @@ window.playDrums = function(tempo){
 		} 
 				
 		if(beatand==1){
-			$('#counter').html(''+counter+'');
+		//	$('#counter').html(''+counter+'');
 			$('.topseq'+counter).css('background', '#5f5');
 			
 				if(document.getElementById('bassbox'+counter).checked) {
 					$("#playbass").prop("currentTime",0);
 					$('#playbass').trigger('play');
+					$('#bass'+counter+'>.indicator').slideDown(10);
+					$('#bass'+counter+'>.indicator').slideUp(200);
 				}
 				
 				if(document.getElementById('snarebox'+counter).checked) {
 					$("#playsnare").prop("currentTime",0);
 					$('#playsnare').trigger('play');
+					$('#snare'+counter+'>.indicator').slideDown(10);
+					$('#snare'+counter+'>.indicator').slideUp(200);
+					
 				}
 				
 				
 				if(document.getElementById('hatbox'+counter).checked) {
 					$("#playhat").prop("currentTime",0);
 					$('#playhat').trigger('play');
+					$('#hat'+counter+'>.indicator').slideDown(10);
+					$('#hat'+counter+'>.indicator').slideUp(200);
 				}
 				
 			}
@@ -158,7 +165,7 @@ window.playDrums = function(tempo){
 		
 		$("#sequencer").append(''+
 		
-	
+			'<div style="float: left; width: 8%;  border: solid 1px #fff; margin-right: 4px;"></div>'+
 			'<div class="topseq topseq1 last"></div>'+
 			'<div class="topseq topseq2 "></div>'+
 			'<div class="topseq topseq3 "></div>'+
@@ -169,67 +176,86 @@ window.playDrums = function(tempo){
 			'<div class="topseq topseq8 "></div>'+
 	
 		
-
-		
-		'<div class="track">'+ 
 		'<div class="track">'+
-			'<div class="institle">Bass Drum</div>'+
-			'<div class="player">'+
-				'<audio ID="playbass" controls> <source src="./bass.mp3" type="audio/mpeg"></audio>'+
-			'<div id="randomz"></div>'+
-			'</div>'+
-		
-			'<div class="seq seq1 last"><label for="bassbox1">1</label><input ID = "bassbox1" class="bassbox" type="checkbox"  name="bassbox1" value="1" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq2 "><label for="bassbox2">2</label><input ID = "bassbox2" class="bassbox" type="checkbox"  name="bassbox2"  value="2" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq3 "><label for="bassbox3">3</label><input ID = "bassbox3" class="bassbox" type="checkbox"  name="bassbox3" value="3" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq4 "><label for="bassbox4">4</label><input ID = "bassbox4" class="bassbox" type="checkbox"  name="bassbox4"  value="4" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq5 "><label for="bassbox5">5</label><input ID = "bassbox5" class="bassbox" type="checkbox"  name="bassbox5" value="5" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq6 "><label for="bassbox6">6</label><input ID = "bassbox6" class="bassbox" type="checkbox"  name="bassbox6"  value="6" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq7 "><label for="bassbox7">7</label><input ID = "bassbox7" class="bassbox" type="checkbox"  name="bassbox7" value="7" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq8 "><label for="bassbox8">8</label><input ID = "bassbox8" class="bassbox" type="checkbox"  name="bassbox8"  value="8" onchange="editNow();"><div class="subdiv"></div></div>'+
-		'</div>'+
-		
-		
-		
-		
-		
-		'<div class="track">'+
-			'<div class="institle">Snare Drum</div>'+
-			'<div class="player">'+
-				'<audio  ID="playsnare" controls > <source src="./snare.mp3" type="audio/mpeg"></audio>'+
-			'<div id="randomy"></div>'+
-			'</div>'+
-			
-			'<div class="seq seq1  last"> <label for="snarebox1">1</label> <input ID = "snarebox1"  class="snarebox" type="checkbox"  name="snarebox1" value="1" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq2 "> <label for="snarebox2">2</label> <input ID = "snarebox2"   class="snarebox" type="checkbox"  name="snarebox2"  value="2" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq3 "> <label for="snarebox3">3</label> <input ID = "snarebox3"   class="snarebox" type="checkbox"  name="snarebox3" value="3" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq4 "> <label for="snarebox4">4</label> <input ID = "snarebox4"   class="snarebox" type="checkbox"  name="snarebox4"  value="4" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq5 "> <label for="snarebox5">5</label> <input ID = "snarebox5"   class="snarebox" type="checkbox"  name="snarebox5" value="5" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq6 "> <label for="snarebox6">6</label> <input ID = "snarebox6"   class="snarebox" type="checkbox"  name="snarebox6"  value="6" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq7 "> <label for="snarebox7">7</label> <input ID = "snarebox7"   class="snarebox" type="checkbox"  name="snarebox7" value="7" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq8 "> <label for="snarebox8">8</label> <input ID = "snarebox8"   class="snarebox" type="checkbox"  name="snarebox8"  value="8" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'</div>'+
-			
-		'<div class="track">'+
-			'<div class="institle">HiHat</div>'+
 			'<div class="player">'+
 				'<audio  ID="playhat" controls><source src="./hihat2.mp3" type="audio/mpeg"></audio>'+
 			'<div id="randomx"></div>'+
 			'</div>'+
+			'<div class="institle seq last" style="float: left; width: 8%;  border: solid 1px #fff;"><img src="hihat.png" width="50px">HiHat</div>'+
 			
-			'<div class="seq  seq1  last"> <label for="hatbox1">1</label> <input ID = "hatbox1" class="hatbox"  type="checkbox"  name="snarebox1" value="1" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq2 "> <label for="hatbox2">2</label> <input ID = "hatbox2" class="hatbox"  type="checkbox"  name="snarebox2"  value="2" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq3 "> <label for="hatbox3">3</label> <input ID = "hatbox3" class="hatbox"  type="checkbox"  name="snarebox3" value="3" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq4 "> <label for="hatbox4">4</label> <input ID = "hatbox4" class="hatbox"  type="checkbox"  name="snarebox4"  value="4" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq5 "> <label for="hatbox5">5</label> <input ID = "hatbox5" class="hatbox"  type="checkbox"  name="snarebox5" value="5" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq6 "> <label for="hatbox6">6</label> <input ID = "hatbox6" class="hatbox"  type="checkbox"  name="snarebox6"  value="6" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq7 "> <label for="hatbox7">7</label> <input ID = "hatbox7" class="hatbox"  type="checkbox"  name="snarebox7" value="7" onchange="editNow();"><div class="subdiv"></div></div>'+
-			'<div class="seq seq8 "> <label for="hatbox8">8</label> <input ID = "hatbox8" class="hatbox" type="checkbox"  name="snarebox8"  value="8" onchange="editNow();"><div class="subdiv"></div></div>'+
+			'<div  ID="hat1" class="seq  seq1  "> <label for="hatbox1">1</label> <input ID = "hatbox1" class="hatbox"  type="checkbox"  name="snarebox1" value="1" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			'<div  ID="hat2" class="seq seq2 "> <label for="hatbox2">2</label> <input ID = "hatbox2" class="hatbox"  type="checkbox"  name="snarebox2"  value="2" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div  ID="hat3" class="seq seq3 "> <label for="hatbox3">3</label> <input ID = "hatbox3" class="hatbox"  type="checkbox"  name="snarebox3" value="3" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div  ID="hat4" class="seq seq4 "> <label for="hatbox4">4</label> <input ID = "hatbox4" class="hatbox"  type="checkbox"  name="snarebox4"  value="4" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			'<div  ID="hat5" class="seq seq5 "> <label for="hatbox5">5</label> <input ID = "hatbox5" class="hatbox"  type="checkbox"  name="snarebox5" value="5" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			'<div  ID="hat6" class="seq seq6 "> <label for="hatbox6">6</label> <input ID = "hatbox6" class="hatbox"  type="checkbox"  name="snarebox6"  value="6" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			'<div  ID="hat7" class="seq seq7 "> <label for="hatbox7">7</label> <input ID = "hatbox7" class="hatbox"  type="checkbox"  name="snarebox7" value="7" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			'<div  ID="hat8" class="seq seq8 "> <label for="hatbox8">8</label> <input ID = "hatbox8" class="hatbox" type="checkbox"  name="snarebox8"  value="8" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+		'</div>'+
+
+
+
+		'<div class="track">'+
+			'<div class="player">'+
+				'<audio  ID="playsnare" controls > <source src="./snare.mp3" type="audio/mpeg"></audio>'+
+			'<div id="randomy"></div>'+
+			'</div>'+
+			'<div class="institle seq last" style="float: left; width: 8%; border: solid 1px #fff;"><img src="snare.png" width="50px"> Snare Drum</div>'+
+			
+			
+			'<div  ID="snare1" class="seq seq1  "> <label for="snarebox1">1</label> <input ID = "snarebox1"  class="snarebox" type="checkbox"  name="snarebox1" value="1" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div ID="snare2" class="seq seq2 "> <label for="snarebox2">2</label> <input ID = "snarebox2"   class="snarebox" type="checkbox"  name="snarebox2"  value="2" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div  ID="snare3" class="seq seq3 "> <label for="snarebox3">3</label> <input ID = "snarebox3"   class="snarebox" type="checkbox"  name="snarebox3" value="3" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div  ID="snare4" class="seq seq4 "> <label for="snarebox4">4</label> <input ID = "snarebox4"   class="snarebox" type="checkbox"  name="snarebox4"  value="4" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div  ID="snare5" class="seq seq5 "> <label for="snarebox5">5</label> <input ID = "snarebox5"   class="snarebox" type="checkbox"  name="snarebox5" value="5" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div  ID="snare6" class="seq seq6 "> <label for="snarebox6">6</label> <input ID = "snarebox6"   class="snarebox" type="checkbox"  name="snarebox6"  value="6" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div  ID="snare7" class="seq seq7 "> <label for="snarebox7">7</label> <input ID = "snarebox7"   class="snarebox" type="checkbox"  name="snarebox7" value="7" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div  ID="snare8" class="seq seq8 "> <label for="snarebox8">8</label> <input ID = "snarebox8"   class="snarebox" type="checkbox"  name="snarebox8"  value="8" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			'</div>'+
+
+	
+		'<div class="track">'+
+			'<div class="player">'+
+				'<audio ID="playbass" controls> <source src="./bass.mp3" type="audio/mpeg"></audio>'+
+			'<div id="randomz"></div>'+
+			'</div>'+
+			'<div class="institle seq last" style="float: left; width: 8%;  border: solid 1px #fff;"><img src="bass.png" width="50px"> Bass Drum</div>'+
+			
+			'<div ID="bass1" class="seq seq1 "><label for="bassbox1">1</label><input ID = "bassbox1" class="bassbox" type="checkbox"  name="bassbox1" value="1" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div ID="bass2"class="seq seq2 "><label for="bassbox2">2</label><input ID = "bassbox2" class="bassbox" type="checkbox"  name="bassbox2"  value="2" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div ID="bass3"class="seq seq3 "><label for="bassbox3">3</label><input ID = "bassbox3" class="bassbox" type="checkbox"  name="bassbox3" value="3" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div ID="bass4" class="seq seq4 "><label for="bassbox4">4</label><input ID = "bassbox4" class="bassbox" type="checkbox"  name="bassbox4"  value="4" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+	
+			'<div ID="bass5"   class="seq seq5 "><label for="bassbox5">5</label><input ID = "bassbox5" class="bassbox" type="checkbox"  name="bassbox5" value="5" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div ID="bass6"class="seq seq6 "><label for="bassbox6">6</label><input ID = "bassbox6" class="bassbox" type="checkbox"  name="bassbox6"  value="6" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div ID="bass7" class="seq seq7 "><label for="bassbox7">7</label><input ID = "bassbox7" class="bassbox" type="checkbox"  name="bassbox7" value="7" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
+			
+			'<div ID="bass8" class="seq seq8 "><label for="bassbox8">8</label><input ID = "bassbox8" class="bassbox" type="checkbox"  name="bassbox8"  value="8" onchange="editNow();"><div class="indicator"></div><div class="subdiv"></div></div>'+
 		'</div>'+
 		
+		
+		
+		
+					
+				
 		'');
 		
-								 
+		
+										 
 	}
 	
 	
